9606fc057355012fbc015d5f139a0d0e88f03bf1
pengo <pbalternates@gmail.com>
2026-04-23 15:06:20 -0600

    feat(packages.kitty): init

---
+++ packages/kitty/package.nix
@@ -0,0 +1,26 @@
+{ inputs, lib, pkgs-self, pkgs-unstable, ... }:
+
+let
+  wrapper = inputs.wrapper-modules.lib.evalModule ./wrapper.nix;
+
+  inherit (lib) getExe;
+
+  inherit (pkgs-unstable) kitty;
+  zsh = pkgs-self.zsh.wrap {
+    zshrc-sources = [(
+      pkgs-unstable.writeText "kitty.zsh" ''
+        # === Kitty Terminal Compat ===
+        alias ssh='${kitty}/bin/kitten ssh'
+      ''
+    )];
+  };
+in
+wrapper.config.wrap {
+  pkgs = pkgs-unstable;
+
+  configFile.content = ''
+    background_opacity         0.5
+    dynamic_background_opacity yes
+    shell                      ${getExe zsh}
+  '';
+}
---
+++ packages/kitty/wrapper.nix
@@ -0,0 +1,25 @@
+{ config, pkgs, lib, wlib, ... }: {
+  imports = [ wlib.modules.default ];
+
+  options = {
+    configFile = lib.mkOption {
+      type = wlib.types.file pkgs;
+      description = ''
+        Config file that kitty will use as its config file.
+      '';
+      default.path = config.constructFiles.generatedConfig.path;
+      default.content = "";
+    };
+  };
+
+  config = {
+    package = lib.mkDefault pkgs.kitty;
+
+    constructFiles.generatedConfig = {
+      relPath = "kitty.conf";
+      content = config.configFile.content or "";
+    };
+
+    flags."--config" = config.configFile.path;
+  };
+}
