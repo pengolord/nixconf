f5be03f3fb6f6d234f19577a8b930ae6eb315c02
pengo <pbalternates@gmail.com>
2026-04-24 09:43:30 -0600

    feat(nixosModules.secrets): init

---
+++ nixos/modules/secrets/module.nix
@@ -0,0 +1,10 @@
+{ inputs, ... }:
+
+{
+  imports = [ inputs.agenix.nixosModules.default ];
+
+  age.identityPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
+  age.secrets = {
+    pengoHashedPasswordFile.file = ../../../secrets/user/hashedPasswordFile.age;
+  };
+}
