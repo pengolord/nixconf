e9f5911482220eb624c18e22508f8ee6b356f97f
pengo <pbalternates@gmail.com>
2026-05-10 01:23:30 -0600

    fix(packages.noctalia): update matugen templates path

--- packages/noctalia/package.nix
+++ packages/noctalia/package.nix
@@ -13,7 +13,7 @@ inputs.wrapper-modules.wrappers.noctalia-shell.wrap (
     outOfStoreConfig = "/home/pengo/.config/noctalia";
 
     constructFiles.user-templates = lib.mkForce {
-      content = import ../matugen/templates;
+      content = import ../../adios-wrappers/matugen/templates;
       relPath = "${config.generatedConfigDirname}/user-templates.toml";
     };
   }
