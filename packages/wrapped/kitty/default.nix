56d5c9dce943f9958b42e775720da19083b26283
pengo <pbalternates@gmail.com>
2026-05-12 23:23:47 -0600

    fix(packages.kitty): use nushell

--- packages/wrapped/kitty/default.nix
+++ packages/wrapped/kitty/default.nix
@@ -1,25 +1,16 @@
 _: {
-  inputs.zsh.path = "/zsh";
-
-  mutations."/zsh".zshrcFiles = {inputs}: let
-    inherit (inputs.nixpkgs.pkgs) kitty writeText;
-  in [
-    (writeText "kitty.zsh" ''
-      # === Kitty Terminal Compat ===
-      alias ssh='${kitty}/bin/kitten ssh'
-    '')
-  ];
+  inputs.nushell.path = "/nushell";
 
   options = {
     configFile.defaultFunc = {inputs}: let
       inherit (inputs.nixpkgs.lib) getExe;
       inherit (inputs.nixpkgs.pkgs) writeText;
-      zsh = inputs.zsh {};
+      nushell = inputs.nushell {};
     in
       writeText "kitty.conf" ''
         background_opacity         0.7
         dynamic_background_opacity yes
-        shell                      ${getExe zsh}
+        shell                      ${getExe nushell}
 
         include ~/.cache/matugen/kitty.conf
       '';
