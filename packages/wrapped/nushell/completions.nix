79ce6d9b70b38d2cedaebc83dbc395a4eaefe978
pengo <pbalternates@gmail.com>
2026-05-16 23:58:21 -0600

    fix(packages.nushell): actually load completions

--- packages/wrapped/nushell/completions.nix
+++ packages/wrapped/nushell/completions.nix
@@ -2,12 +2,39 @@
   lib,
   pkgs,
 }: ''
+  $env.PATH = ($env.PATH | split row (char esep) | where { $in != "/home/pengo/.config/carapace/bin" } | prepend "/home/pengo/.config/carapace/bin")
+
+  def --env get-env [name] { $env | get $name }
+  def --env set-env [name, value] { load-env { $name: $value } }
+  def --env unset-env [name] { hide-env $name }
+
   let carapace_completer = {|spans|
-    ${lib.getExe pkgs.carapace} $spans.0 nushell ...$spans | from json
+    load-env {
+      CARAPACE_SHELL_BUILTINS: (help commands | where category != "" | get name | each { split row " " | first } | uniq  | str join "\n")
+      CARAPACE_SHELL_FUNCTIONS: (help commands | where category == "" | get name | each { split row " " | first } | uniq  | str join "\n")
+    }
+
+    # if the current command is an alias, get it's expansion
+    let expanded_alias = (scope aliases | where name == $spans.0 | $in.0?.expansion?)
+
+    # overwrite
+    let spans = (if $expanded_alias != null  {
+      # put the first word of the expanded alias first in the span
+      $spans | skip 1 | prepend ($expanded_alias | split row " " | take 1)
+    } else {
+      $spans | skip 1 | prepend ($spans.0)
+    })
+
+    ${lib.getExe pkgs.carapace} $spans.0 nushell ...$spans
+    | from json
   }
 
-  $env.config.completions.external = {
-    enable: true
-    completer: $carapace_completer
-  }
+  mut current = (($env | default {} config).config | default {} completions)
+  $current.completions = ($current.completions | default {} external)
+  $current.completions.external = ($current.completions.external
+  | default true enable
+  # backwards compatible workaround for default, see nushell #15654
+  | upsert completer { if $in == null { $carapace_completer } else { $in } })
+
+  $env.config = $current
 ''
